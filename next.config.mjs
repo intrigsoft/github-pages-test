6831705d5be1014e4d2be0d28bb7d10d480f22c8
tiran <tiran@intrigsoft.com>
2025-03-30 14:24:33 +0530

    Add postbuild script and update Next.js config for export
    Added a `postbuild` script to ensure GitHub Pages compatibility by creating a `.nojekyll` file. Updated `next.config.mjs` to conditionally set `basePath` and `assetPrefix` for production, and enabled `trailingSlash` for better URL handling.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 import withMarkdoc from '@markdoc/next.js'
 
 import withSearch from './src/markdoc/search.mjs'
+const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -8,6 +9,7 @@ const nextConfig = {
   output: "export",
   basePath: isProd ? '/github-pages-test' : '',
   assetPrefix: isProd ? '/github-pages-test/' : '',
+  trailingSlash: true,
 }
 
 export default withSearch(
